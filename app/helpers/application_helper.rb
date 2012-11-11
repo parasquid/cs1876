5a22dd76e8ccd610fb143f47fc4b4faddea29374
Tristan <tristan.gomez@gmail.com>
2012-11-11 18:06:36 +0800

    add icons

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def beds(numbeds)
+    "<span style='inline; margin: 0 4px 0 24px;'>#{image_tag 'noun_project_1072.svg', size: '18x14' } #{numbeds}</span>".html_safe
+  end
+  def baths(numbaths)
+    "<span style='inline; margin: 0 4px;'>#{image_tag 'noun_project_1718.svg', size: '18x14' } #{numbaths}</span>".html_safe
+  end
+  def cars(numcars)
+    "<span style='inline; margin: 0 4px;'>#{image_tag 'noun_project_4412.svg', size: '18x14' } #{numcars}</span>".html_safe
+  end
 end
