3b633e086bbcd17372bd6fcd31fa70bc31a194b7
Tristan <tristan.gomez@gmail.com>
2012-11-11 18:15:37 +0800

    index page design fixes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def beds(numbeds)
-    "<span style='inline; margin: 0 4px 0 24px;'>#{image_tag 'noun_project_1072.svg', size: '18x14' } #{numbeds}</span>".html_safe
+    "<span style='inline; margin: 0 4px'>#{image_tag 'noun_project_1072.svg', size: '18x14' } #{numbeds}</span>".html_safe
   end
   def baths(numbaths)
     "<span style='inline; margin: 0 4px;'>#{image_tag 'noun_project_1718.svg', size: '18x14' } #{numbaths}</span>".html_safe
